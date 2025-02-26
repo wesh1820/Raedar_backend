05a5410b5068af85a675e97191a4af652e88e437
Wesley Roekens <Roekenswesley@gmail.com>
2025-02-26 18:19:06 +0100

    No firebase anymore but mongoose authentication

--- models/User.js
+++ models/User.js
@@ -1,9 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+// Create a schema for the User model
 const userSchema = new mongoose.Schema({
-  firebaseUID: { type: String, required: true },
-  email: { type: String, required: true },
-  name: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true, // Ensure email is unique
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('User', userSchema);
+// Create the User model based on the schema
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
