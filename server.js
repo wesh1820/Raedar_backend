e6660fc81af27477733ca8b7b589de35419dea7c
Wesley Roekens <Roekenswesley@gmail.com>
2025-02-26 19:15:13 +0100

    No firebase anymore but mongoose authentication because it still doesnt work

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const bcrypt = require("bcryptjs");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("./models/User");
